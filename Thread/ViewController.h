6262d3060e6575dd3f7c1ed954c7e1f21723dc90
Bsetecip10 <bsetecip10@Nirmals-Mac-mini.local>
2014-11-25 17:24:16 +0530

    Initial Commit

---
+++ Thread/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Thread
+//
+//  Created by Bsetecip10 on 25/11/14.
+//  Copyright (c) 2014 gyana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Thread/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Thread
+//
+//  Created by Bsetecip10 on 25/11/14.
+//  Copyright (c) 2014 gyana. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
